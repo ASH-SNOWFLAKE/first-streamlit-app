9a2ddf2664def1617902930ee2143ac7116c01ad
ASH-SNOW <122273825+ASH-SNOWFLAKE@users.noreply.github.com>
2023-01-09 14:05:52 +0000

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My moms new healthy diner')
 streamlit.header('Breakfast Favourites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Bolied Free-Ranged Egg')
-streamlit.text('Avo Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Bolied Free-Ranged Egg')
+streamlit.text('🥑🍞 Avo Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
