8e00a847287a2532737c7d145e608f2ffe18f17d
ASH-SNOW <122273825+ASH-SNOWFLAKE@users.noreply.github.com>
2023-01-10 16:34:17 +0000

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# similify record 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# apply it
+streamlit.dataframe(fruityvice_normalized)
